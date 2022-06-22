ae9118cb1abba85559895b54f18fc95b28c68c5d
Ninganna Kuntoji <ninganna2102@gmail.com>
2022-06-22 17:54:54 +0530

    Added android project

---
+++ app/src/main/java/com/nrk/pillreminder/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.nrk.pillreminder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
